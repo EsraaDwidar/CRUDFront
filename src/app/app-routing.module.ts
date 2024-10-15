0b86380cb5253541af973682d38ef2491e4ada81
EsraaDwidar <68755328+EsraaDwidar@users.noreply.github.com>
2024-10-15 03:26:08 +0300

    Update Delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoItemComponent } from './components/Todo/todo-item/todo-item.component';
 import { TodoComponent } from './components/Todo/todo.component';
-import { DeleteItemComponent } from './components/Todo/delete-item/delete-item.component';
 
 const routes: Routes = [
   {path:"todo", component: TodoComponent},
   {path:"", component: TodoComponent},
-  {path:"todoitem/:id",component: TodoItemComponent},
-  {path:"delete/:id",component: DeleteItemComponent}
+  {path:"todoitem/:id",component: TodoItemComponent}
 ];
 
 @NgModule({
